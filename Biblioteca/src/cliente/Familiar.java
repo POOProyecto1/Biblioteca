c321e5ee7c8760d8f3bd41e85a3b5df05dcdd908
Jose Ruiz <jruizj6@hotmail.com>
2014-09-06 23:37:34 -0600

    Edición de clase Familiar y Creación de paquete objetos

--- Biblioteca/src/cliente/Familiar.java
+++ Biblioteca/src/cliente/Familiar.java
@@ -1,24 +1,28 @@
-
+package cliente;
 
 public class Familiar extends Persona {
 	private String parentesco;
-	public void Familiar() {
 	
-	}
-	
-	public boolean setParentesco(String parentesco) {
-	
+	public Familiar(String nombre,String primerApellido,String segundoApellido,
+			String edad,String direccion,String telefono,String correo,
+			String carné,String parentesco) {
+			super(nombre,primerApellido,segundoApellido,edad,direccion,telefono,correo);
+			setParentesco(parentesco);
 	}
 	
 	public String getParentesco() {
-	
+		return parentesco;
 	}
-	
-	public void getString() {
-	
+
+	public void setParentesco(String parentesco) {
+		this.parentesco = parentesco;
+	}
+
+	/*public void getString() {
+	//TODO
 	}
 	
 	public String toString() {
-	
-	}
+	//TODO
+	}*/
 }
