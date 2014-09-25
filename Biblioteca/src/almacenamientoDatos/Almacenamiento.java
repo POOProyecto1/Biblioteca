67b93ddb47dfabc38f04b33b3d2157c7a54236c0
Jose Ruiz <jruizj6@hotmail.com>
2014-09-24 22:43:52 -0600

    La función guardar fue modificada para almacenar un ArrayList de Personas.

--- Biblioteca/src/almacenamientoDatos/Almacenamiento.java
+++ Biblioteca/src/almacenamientoDatos/Almacenamiento.java
@@ -2,15 +2,16 @@ package almacenamientoDatos;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.ObjectOutputStream;
+import java.util.ArrayList;
 
 import cliente.Persona;
  
 public class Almacenamiento {
 	
-	public boolean guardar( Persona persona, int numero){
+	public boolean guardar( ArrayList<Persona>persona, int numero){
 		FileOutputStream salida= null;
 		ObjectOutputStream objeto=null;
-		File nameArchivo=new File("persona"+numero+".txt");
+		File nameArchivo=new File("src/DatosPersonas","persona"+numero+".txt");
 		try{
 			salida= new FileOutputStream(nameArchivo);
 			objeto=new ObjectOutputStream(salida);
