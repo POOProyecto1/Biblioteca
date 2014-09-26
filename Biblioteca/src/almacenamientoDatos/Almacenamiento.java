6f90d19a0b0f607481ed2f05b9b6563b5af15233
Jose Ruiz <jruizj6@hotmail.com>
2014-09-25 21:38:57 -0600

    Creación de la función cargar

--- Biblioteca/src/almacenamientoDatos/Almacenamiento.java
+++ Biblioteca/src/almacenamientoDatos/Almacenamiento.java
@@ -1,30 +1,34 @@
 package almacenamientoDatos;
+
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
-
-import cliente.Persona;
+import java.io.FileInputStream; 
+import java.io.ObjectInputStream; 
  
 public class Almacenamiento {
 	
-	public boolean guardar( ArrayList<Persona>persona, int numero){
+	public boolean guardar( ArrayList<Object> lista, String tipo){
 		FileOutputStream salida= null;
 		ObjectOutputStream objeto=null;
-		File nameArchivo=new File("src/DatosPersonas","persona"+numero+".txt");
+		File nameArchivo=new File("src/Datos","lista-"+tipo+".txt");
 		try{
 			salida= new FileOutputStream(nameArchivo);
 			objeto=new ObjectOutputStream(salida);
 			
-			objeto.writeObject(persona);
+			objeto.writeObject(lista);
 		}
 		catch(Exception e){return false;}
 		finally{
 			try {
 				if (objeto!=null){
 					objeto.close();
+					System.out.println("\nEl archivo lista-"+tipo+".txt fue guardado exitosamente.");
 					return true;
 				};
+				System.out.println("Ha ocurrido un error al guardar los datos.");
 				return false;
 			}
 			catch (Exception e2) {}
@@ -32,5 +36,35 @@ public class Almacenamiento {
 		return true;
 	};
 	
+	public ArrayList<Object> cargar(String tipo){
+		 ArrayList<Object>lista = new ArrayList<Object>();
+		 
+		 File nameArchivo=new File("src/Datos","lista-"+tipo+".txt");
+		 FileInputStream entrada = null;
+		 ObjectInputStream objeto = null;
+		 
+		 try{
+			 entrada=new FileInputStream(nameArchivo);
+			 objeto=new ObjectInputStream(entrada);
+			 lista=(ArrayList<Object>)objeto.readObject();
+		 }catch(Exception e){}
+		 finally{
+			 try{
+				 if(objeto!=null){
+					objeto.close();
+					System.out.println("El archivo lista-"+tipo+".txt fue cargado con éxito.");
+					return lista;
+				 }
+				 System.out.println("Ha ocurrido un error al cargar los datos.");
+				 return lista;
+			 }
+			 catch( Exception e2 ){}
+		 }
+		
+		return lista;
+		 
+	};
 	
 }
+	
+
