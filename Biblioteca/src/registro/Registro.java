2e6e8ab53e8414a1975237e274bc9e5cfdddea91
Jant1921 <jruizj6@hotmail.com>
2014-09-28 16:24:01 -0600

    Cambios para envio de correos

--- Biblioteca/src/registro/Registro.java
+++ Biblioteca/src/registro/Registro.java
@@ -9,8 +9,8 @@ import enviarMail.javamail;
 import almacenamientoDatos.Almacenamiento;
 
 public class Registro {
-	ArrayList<Object> personas=new ArrayList<Object>();
-	ArrayList<Object> articulos=new ArrayList<Object>();
+	public static ArrayList<Object> personas=new ArrayList<Object>();
+	public static ArrayList<Object> articulos=new ArrayList<Object>();
 	Almacenamiento datos=new Almacenamiento();
 	javamail sendMail=new javamail();
 	
@@ -38,12 +38,16 @@ public class Registro {
 	
 	public void notificar(){
 		int contador=0;
+		personas=getPersonas();
+		Persona cliente;
+		System.out.println(personas.size());
 		System.out.println("Enviando Notificaiones:");
 		for (int i = 0; i < personas.size(); i++) {
-			Persona cliente=((Persona) personas.get(i));
-			if(cliente.getMorosidad()==true){
-				System.out.println("Resultado de envío a"+cliente.getNombre()+" "+cliente.getPrimerApellido()+" "+cliente.getSegundoApellido()+": ");
-				boolean resultado=sendMail.enviarCorreo(cliente.getCorreo(), "Aviso de Morosidad", "Estimad@ "+cliente.getNombre()+" se le recuerda que el articulo "+cliente.getPrestamo()+" se encuentra vencido.\nSe le solicita devolverlo lo más pronto posible.\nAtte: Virtualiteca\nGracias.");
+			cliente=((Persona) personas.get(i));
+			System.out.println(i);
+			if(cliente.verificarMorosidad()==true){
+				System.out.println("Resultado de envÃ­o a "+cliente.getNombre()+" "+cliente.getPrimerApellido()+" "+cliente.getSegundoApellido()+": ");
+				boolean resultado=sendMail.enviarCorreo(cliente.getCorreo(), "Aviso de Morosidad", "Estimad@ "+cliente.getNombre()+" se le recuerda que el articulo "+cliente.getPrestamo()+" se encuentra vencido.\nSe le solicita devolverlo lo mÃ¡s pronto posible.\nAtte: Virtualiteca\nGracias.");
 				System.out.println(resultado);
 				if(resultado==true){contador+=1;};
 			}
@@ -51,21 +55,23 @@ public class Registro {
 		System.out.println("Se han enviado exitosamente "+contador+" notificaciones.");
 	}
 
-	public ArrayList<Object> getPersonas() {
+	public static ArrayList<Object> getPersonas() {
 		return personas;
 	}
 
-	public void setPersonas(ArrayList<Object> personas) {
-		this.personas = personas;
+	public static void setPersonas(ArrayList<Object> personas) {
+		Registro.personas = personas;
 	}
 
-	public ArrayList<Object> getArticulos() {
+	public static ArrayList<Object> getArticulos() {
 		return articulos;
 	}
 
-	public void setArticulos(ArrayList<Object> articulos) {
-		this.articulos = articulos;
+	public static void setArticulos(ArrayList<Object> articulos) {
+		Registro.articulos = articulos;
 	}
+
+
 	
 	
 
