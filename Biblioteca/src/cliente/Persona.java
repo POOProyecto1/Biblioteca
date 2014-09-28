d342e50b9b0484f448df306b74d8aa27918984ab
Jant1921 <jruizj6@hotmail.com>
2014-09-28 15:31:22 -0600

    Update Persona.java

--- Biblioteca/src/cliente/Persona.java
+++ Biblioteca/src/cliente/Persona.java
@@ -1,15 +1,14 @@
 package cliente;
 
 import java.util.ArrayList;
-import objetos.Articulo;	//Se importa la clase Articulo, ya que una persona puede tener o no artículos en préstamo
+import objetos.Articulo;	//Se importa la clase Articulo, ya que una persona puede tener o no artÃ­culos en prÃ©stamo
 import java.io.Serializable;
 
 
 public class Persona implements Serializable {    //se implementa Serializable, ya que es necesario a la hora de guardar una instancia en un txt
 	private static final long serialVersionUID = 1L;
-// Los atributos est�n en protected para que puedan ser accedidos por las clases hijas o clases en el mismo paquete
+// Los atributos están en protected para que puedan ser accedidos por las clases hijas o clases en el mismo paquete
 	protected String edad;
-	protected String direccion;
 	protected String prestamo;  //guarda el nombre del articulo que tiene el prestamo
 	protected String nombre;
 	protected String primerApellido;
@@ -17,14 +16,12 @@ public class Persona implements Serializable {    //se implementa Serializable, 
 	protected String telefono;
 	protected String correo;
 	protected boolean morosidad;
-	protected String cedula;
 	
 	//Constructor de la clase
 	public Persona(String nombre,String primerApellido,String segundoApellido,
-			String edad,String direccion,String telefono,String correo,String cedula) {
-		//Asignación de valores a los atributos
+			String edad,String telefono,String correo) {
+		//AsignaciÃ³n de valores a los atributos
 		setEdad(edad);
-		setDireccion(direccion);
 		prestamo="";
 		setNombre(nombre);
 		setPrimerApellido(primerApellido);
@@ -32,8 +29,7 @@ public class Persona implements Serializable {    //se implementa Serializable, 
 		setTelefono(telefono);
 		setCorreo(correo);
 		setMorosidad(false); 
-		setCedula(cedula);
-	}
+		}
 	
 
 	//Set y get de todos los atributos
@@ -47,20 +43,12 @@ public class Persona implements Serializable {    //se implementa Serializable, 
 	}
 
 
-	public String getDireccion() {
-		return direccion;
-	}
-
-
-	public void setDireccion(String direccion) {
-		this.direccion = direccion;
-	}
-
+	
 	public String getPrestamo() {
 		return prestamo;
 	}
 
-	//Agrega un nuevo artículo a los que tiene el usuario actualmente en préstamo.
+	//Agrega un nuevo artÃ­culo a los que tiene el usuario actualmente en prÃ©stamo.
 	public void setPrestamo(String prestamo) {
 		this.prestamo=prestamo;			
 	}
@@ -129,22 +117,15 @@ public class Persona implements Serializable {    //se implementa Serializable, 
 		return morosidad;
 	}
 	
-	public String getCedula() {
-		return cedula;
-	}
-
-	public void setCedula(String cedula) {
-		this.cedula = cedula;
-	}
 
 	//Funcion verificar correo: analiza el correo ingresado para evitar problemas a la hora de enviar notificaciones.
 	//Se crea static, ya que es necesario 
 	public static boolean verificarCorreo(String correo){   
 		if(correo.contains("@")){     //busca que el correo contenga un "@"
 			String[] temp=correo.split("@");	//separa el correo en dos partes mediante el "@"
-			if(temp[0].length()!=0){			//si esta condici�n se cumple significa que existe un nombre de usuario antes del "@"
+			if(temp[0].length()!=0){			//si esta condición se cumple significa que existe un nombre de usuario antes del "@"
 				if (temp[1].contains(".")){		//verifica que exista un "." al lado derecho del "@"
-					if (temp[1].startsWith(".")){ //si esta condici�n se cumple significa que no existe nada entre "@" y "."
+					if (temp[1].startsWith(".")){ //si esta condición se cumple significa que no existe nada entre "@" y "."
 						return false;
 					}
 					else{						//si existe un dominio entre "@" y "."
@@ -185,13 +166,12 @@ public class Persona implements Serializable {    //se implementa Serializable, 
 		}
 	}
 	
-	public String toString() { // Impresión de toda la información personal del prestatario
+	public String toString() { // ImpresiÃ³n de toda la informaciÃ³n personal del prestatario
 		String msg = "";
 		
 		msg = "Nombre Completo: " + getNombre() +" "+ getPrimerApellido() +" "+ getSegundoApellido() + "\n";
 		msg += "Edad: " + getEdad() + "\n";
-		msg += "Direcci�n exacta: " + getDireccion() + "\n";
-		msg += "Tel�fono: " + getTelefono() + "\n";
+		msg += "Teléfono: " + getTelefono() + "\n";
 		msg += "Email de contacto: " + getCorreo() + "\n";
 		msg += "Moroso :" + getMorosidad() + "\n";
 		return msg;
