392c3d8d39201b1c203139c06f3ac0ed8437e797
Jant1921 <jruizj6@hotmail.com>
2014-09-28 20:01:46 -0600

    Documentación de codigo

--- Biblioteca/src/cliente/Colega.java
+++ Biblioteca/src/cliente/Colega.java
@@ -1,39 +1,42 @@
 package cliente;
 
+//Clase publica colega que hereda atributos y métodos de la clase Persona
 public class Colega extends Persona {
-	private static final long serialVersionUID = 1L;
-	private String puesto;
-	private String lugarTrabajo;
-	private String tipo = "colega";
+	private static final long serialVersionUID = 1L;    //codigo de identificaión
+	private String puesto;				//puesto del colega
+	private String lugarTrabajo;			//lugar en donde labora
+	private String tipo = "colega";			
 	
+	//Constructor
 	public Colega(String nombre,String primerApellido,String segundoApellido,
 			String edad,String telefono,String correo,String puesto,String lugarTrabajo) {
 		super( nombre, primerApellido, segundoApellido,
-			 edad,telefono, correo);
-		setPuesto(puesto);
+			 edad,telefono, correo);			//se envían algunos atributos al constructor de la superclase
+		setPuesto(puesto);					
 		setLugarTrabajo(lugarTrabajo);
 	}
 		
+	//funcion que retorna el puesto del colega	
 	public String getPuesto() {
 		return puesto;
 	}
-
+	//método que le esteblece un valor al atributo puesto 
 	public void setPuesto(String puesto) {
 		this.puesto = puesto;
 	}
-
+	//funcion que retorna el lugar de trabajo
 	public String getLugarTrabajo() {
 		return lugarTrabajo;
 	}
-
+	//método que le esteblece un valor al atributo lugarTrabajo
 	public void setLugarTrabajo(String lugarTrabajo) {
 		this.lugarTrabajo = lugarTrabajo;
 	}
-	
+	//funcion que retorna el tipo de la clase
 	public String getTipo(){
 		return tipo;
 	}
-	
+	//funcion que retorna el objeto en forma de String
 	public String toString() {
 		String msg=super.toString();
 		msg+="Lugar de Trabajo: "+getLugarTrabajo()+"\n";
