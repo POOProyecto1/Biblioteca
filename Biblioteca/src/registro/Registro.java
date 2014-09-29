d56935bbc313c561ed55118ec202e55f7d8ee437
Jant1921 <jruizj6@hotmail.com>
2014-09-28 21:44:29 -0600

    Documentación de codigo

--- Biblioteca/src/registro/Registro.java
+++ Biblioteca/src/registro/Registro.java
@@ -1,5 +1,5 @@
 package registro;
-
+//se importan los elementos necesarios para el correcto funcionamiento de la clase
 import java.util.ArrayList;
 import java.util.Collections;
 
@@ -10,13 +10,23 @@ import enviarMail.javamail;
 
 import almacenamientoDatos.Almacenamiento;
 
+//clase Registro implementa de Comparable
 public class Registro implements Comparable {
-	public static ArrayList<Object> personas=new ArrayList<Object>();
-	public static ArrayList<Object> articulos=new ArrayList<Object>();
-	Almacenamiento datos=new Almacenamiento();
-	javamail sendMail=new javamail();
+	public static ArrayList<Object> personas=new ArrayList<Object>();		//lista que contiene a las personas
+	public static ArrayList<Object> articulos=new ArrayList<Object>();		//lista que contiene los articulos
+	Almacenamiento datos=new Almacenamiento();					//instancia de Amacenamiento
+	javamail sendMail=new javamail();						//Instancia de javamail
+	
+
+	public Registro() {
+		leer();
+		notificar();
+	}
 	
 	// Ordenamiento basado en un código de Internet
+	//Encargada de comparar objetos para ordenarlos
+	//recibe un objeto
+	//retorna int
 	public int compareTo(Object cmp) {
 		int order = 0;
         Articulo articulo = (Articulo)cmp;        
@@ -29,34 +39,43 @@ public class Registro implements Comparable {
        return order; 
     }
 	
-	public Registro() {
-		leer();
-		notificar();
-	}
-	
+	/*
+	funcion  guardar
+	se encarga de guardar un ArrayList en un .txt
+	no recibe parámetros
+	devuelve boolean
+	*/
 	public boolean guardar(){
 		boolean resultadoP= datos.guardar(getPersonas(), "Personas");
 		boolean resultadoA=datos.guardar(getArticulos(), "Articulos");
-		if(resultadoP==true&&resultadoA==true){
+		if(resultadoP==true&&resultadoA==true){ //verifica que ambos procesos hayan finalizado correctamente
 			return true;
 		}else{
 			return false;
 		}
 	}
-	
+	/*
+	método leer
+	se encarga de leer datos desde un .txt
+	y almacenarlos en sus respectivas listas
+	*/
 	public void leer(){
 		setArticulos(datos.cargar("Articulos"));
 		setPersonas(datos.cargar("Personas"));		
 	}
 	
+	/*
+	Método notificar
+	Encargado de verificar el estado de morosidad de todas las personas y enviar correos electronicos si estas tienen morosidad
+	*/
 	public void notificar(){
 		int contador=0;
-		personas=getPersonas();
+		personas=getPersonas();		//carga la lista de personas en una variable local
 		Persona cliente;
 		System.out.println("Enviando Notificaiones:");
-		for (int i = 0; i < personas.size(); i++) {
+		for (int i = 0; i < personas.size(); i++) {		//revisa una por una a las personas
 			cliente=((Persona) personas.get(i));
-			if(cliente.verificarMorosidad()==true){
+			if(cliente.verificarMorosidad()==true){		//si la persona es morosa, se procede a enviarle un correo electrónico
 				System.out.println("Resultado de envío a "+cliente.getNombre()+" "+cliente.getPrimerApellido()+" "+cliente.getSegundoApellido()+": ");
 				boolean resultado=sendMail.enviarCorreo(cliente.getCorreo(), "Aviso de Morosidad", "Estimad@ "+cliente.getNombre()+" se le recuerda que el articulo "+cliente.getPrestamo()+" se encuentra vencido.\nSe le solicita devolverlo lo más pronto posible.\nAtte: Virtualiteca\nGracias.");
 				System.out.println(resultado);
@@ -65,7 +84,10 @@ public class Registro implements Comparable {
 		}
 		System.out.println("Se han enviado exitosamente "+contador+" notificaciones.");
 	}
-
+	
+	/////////////////////
+	// Setter y Getters//
+	/////////////////////
 	public static ArrayList<Object> getPersonas() {
 		return personas;
 	}
