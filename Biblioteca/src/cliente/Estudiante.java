e12862f34a398fab1aea3ec9b29c84e0d4a97add
Jant1921 <jruizj6@hotmail.com>
2014-09-28 20:17:39 -0600

    Documentación de codigo

--- Biblioteca/src/cliente/Estudiante.java
+++ Biblioteca/src/cliente/Estudiante.java
@@ -1,12 +1,12 @@
 package cliente;
 
-
+//clase Cliente que hereda de la clase Persona
 public class Estudiante extends Persona {
-	private static final long serialVersionUID = 1L;
-	private String carne;
-	private String lugarEstudio;
-	private String carrera;
-	private String tipo = "estudiante";
+	private static final long serialVersionUID = 1L;  //identiifcador de la clase, necesario para guardarlo en un .txt
+	private String carne;			//numero de carné
+	private String lugarEstudio;		
+	private String carrera;			//carrera del estudiante
+	private String tipo = "estudiante";	//tipo de la clase
 	
 	//Constructor de la clase
 	public Estudiante(String nombre,String primerApellido,String segundoApellido,
@@ -17,38 +17,40 @@ public class Estudiante extends Persona {
 		setLugarEstudio(lugarEstudio);
 		setCarrera(carrera);
 	}
-	
+	 
+	//funcion que retorna el valor del atributo carne
 	public String getCarne() {
 		return carne;
 	}
-
+	//método que le esteblece un valor al atributo carne
 	public void setCarne(String carne) {
 		this.carne = carne;
 	}
-
+	
+	//funcion que retorna el valor del atributo lugarEstudio
 	public String getLugarEstudio() {
 		return lugarEstudio;
 	}
-
+	//método que le esteblece un valor al atributo lugarEstudio
 	public void setLugarEstudio(String lugarEstudio) {
 		this.lugarEstudio = lugarEstudio;
 	}
-
+	//funcion que retorna el valor del atributo carrera
 	public String getCarrera() {
 		return carrera;
 	}
-
+	//método que le esteblece un valor al atributo carrera
 	public void setCarrera(String carrera) {
 		this.carrera = carrera;
 	}
-	
+	//funcion que retorna el valor del atributo tipo
 	public String getTipo(){
 		return tipo;
 	}
 	
-	public String toString() { // Impresión de toda la información personal del prestatario
+	public String toString() { // ImpresiĂłn de toda la informaciĂłn personal del prestatario
 		String msg = super.toString();
-		msg+="Carn�: "+getCarne()+"\n";
+		msg+="Carné: "+getCarne()+"\n";
 		msg+="Lugar de Estudio: "+getLugarEstudio()+"\n";
 		msg+="Carrera: "+getCarrera()+"\n";
 		return msg;
